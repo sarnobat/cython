c781428dc2ef5c18397193b0ffd79880e1c23469
Sridhar Sarnobat <ss401533@gmail.com>
2023-09-02 22:04:06 -0700

    Minor changes (untested)

--- 4_mwk_neural_network/classify.py
+++ 4_mwk_neural_network/classify.py
@@ -6,26 +6,17 @@ import sys
 df = pd.read_csv ('/tmp/vocabulary.csv',header=None)
 df.rename(columns = {0:'code', 1: 'term'}, inplace = True)
   
-# df = pd.DataFrame(csv_data, columns=['code','term'])
-
-# print(csv_data)
 print(df)
 
 csv_data = []
-csv_reader = csv.DictReader(sys.stdin, fieldnames=['filepath','terms'])
+csv_reader = csv.DictReader(sys.stdin, fieldnames=['filepath'])
 
 for row in csv_reader:
-#     csv_data.append(row)
-# 	r = row.values()
 	print(row['filepath'])
-	print(row['terms'])
-# 	coderow = df['term'].eq('aaron')
+	for term in row.values():
+		print("lll ",term)
 	
 	result_df = df.loc[df['term'] == row['terms']]
 	if len(result_df) > 0:	
 		print(result_df.code.values[0])
 	print("--------------------")
-# 	for term in row['terms']:
-# 		coderow = df['term'].eq(term)
-# 		print(coderow)
-# 	pass
